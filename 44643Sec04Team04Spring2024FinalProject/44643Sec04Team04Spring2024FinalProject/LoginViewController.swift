742182a8e093fc257bd22377e2f6c7b7465b263a
Dharani-puppala <dharanipuppala7@gmail.com>
2024-02-20 16:44:37 -0600

    Dharani Puppala: added lottie animation and added buttons

--- 44643Sec04Team04Spring2024FinalProject/44643Sec04Team04Spring2024FinalProject/LoginViewController.swift
+++ 44643Sec04Team04Spring2024FinalProject/44643Sec04Team04Spring2024FinalProject/LoginViewController.swift
@@ -6,10 +6,26 @@
 //
 
 import UIKit
+import Lottie
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var LaunchLAV: UIView!
+    @IBOutlet weak var LaunchLAV:LottieAnimationView!{
+    
+    didSet{
+        LaunchLAV.loopMode = .playOnce
+        LaunchLAV.animationSpeed = 1.0
+        LaunchLAV.play { [weak self] _ in
+            UIViewPropertyAnimator.runningPropertyAnimator(
+                withDuration: 1.0,
+                delay: 0.0,
+                options:[.curveEaseInOut]){
+                    self?.LaunchLAV.alpha = 0.0
+                }
+        }
+    }
+}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
