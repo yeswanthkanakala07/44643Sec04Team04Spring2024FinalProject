034c54a700533be05d6166c10b066a10078d2b0f
Dharani-puppala <dharanipuppala7@gmail.com>
2024-03-06 11:41:19 -0600

    Dharani Puppala: added register view controller and its connection

---
+++ 44643Sec04Team04Spring2024FinalProject/44643Sec04Team04Spring2024FinalProject/RegisterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterViewController.swift
+//  44643Sec04Team04Spring2024FinalProject
+//
+//  Created by Puppala,Dharani on 3/6/24.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var reenterTF: UITextField!
+    
+    @IBAction func signup(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
