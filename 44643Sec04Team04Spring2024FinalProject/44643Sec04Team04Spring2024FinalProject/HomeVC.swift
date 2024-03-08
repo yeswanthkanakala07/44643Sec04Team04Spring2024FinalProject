001b057b6f8acd6fd5c2012a60df85653e9ef623
vani battu <s567012@nwmissouri.edu>
2024-03-08 15:44:05 -0600

    Added view controller files for home, mystats, my tournaments and profile screens

---
+++ 44643Sec04Team04Spring2024FinalProject/44643Sec04Team04Spring2024FinalProject/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  44643Sec04Team04Spring2024FinalProject
+//
+//  Created by vani battu on 3/8/24.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
