98229bdbd1fbf45277f9c1271f23715c26074856
Dharani-puppala <dharanipuppala7@gmail.com>
2024-03-29 19:42:48 -0500

    Dharani Puppala: addded constratints and uiveiw controllers

---
+++ 44643Sec04Team04Spring2024FinalProject/44643Sec04Team04Spring2024FinalProject/matchteamtournamentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  matchteamtournamentViewController.swift
+//  44643Sec04Team04Spring2024FinalProject
+//
+//  Created by Dharani on 3/29/24.
+//
+
+import UIKit
+
+class matchteamtournamentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
